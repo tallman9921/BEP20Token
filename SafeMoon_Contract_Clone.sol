d1ffa52101c054885fb83fe59b0e244434a239d8
tallman9921 <yemelyan.yevdokimov.domain1121@gmail.com>
2021-07-08 13:47:35 +0700

    Update SafeMoon_Contract_Clone.sol

--- SafeMoon_Contract_Clone.sol
+++ SafeMoon_Contract_Clone.sol
@@ -4,5 +4,6 @@ You need contract like SafeMoon with features:
    2% fee auto distribute to all holders
    I created a black hole so token will deflate itself in supply with every transaction
    50% Supply is burned at start.
+   Auto liquid to Pancake v2.
    
    Please contact me on telegram @TokoAgus
